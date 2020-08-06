d6d173b9401422a0fea65a69de86b5994d4b25d6
Tom <thomas.spencer@codurance.com>
2020-08-07 00:47:54 +0100

    Initial commit

---
+++ src/test/java/com/codurance/RoverShould.java
@@ -0,0 +1,26 @@
+package com.codurance;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class RoverShould {
+
+  private Rover rover;
+
+  @BeforeEach
+  void setUp() {
+    rover = new Rover();
+  }
+
+  @ParameterizedTest
+  @CsvSource({
+      "R, 0:0:E"
+  })
+  void rotate_right(String input, String output) {
+    assertEquals(output, rover.execute(input));
+  }
+}
